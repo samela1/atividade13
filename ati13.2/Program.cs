226290859ad30e3508d3f1e3590222999bb723de
samela1 <sameladejesus@gmail.com>
2019-06-14 21:14:21 -0300

    commit inicia aq

---
+++ ati13.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ati13._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhamatriz;
+            float notas;
+            Console.WriteLine("Quantidades de diciplinas ");
+            linhamatriz = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[linhamatriz , 4 ];
+
+            for (int linha = 1; linha <= linhamatriz; linha++ )
+            {
+                for (int coluna = 0; coluna <= 4; coluna++ )
+                {
+                    Console.Write("Digite suas notas seguencialmente  "  +  ( linha )  +  "  unidade  "   +  ( coluna ));
+                    notas =
+                        float.Parse(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
